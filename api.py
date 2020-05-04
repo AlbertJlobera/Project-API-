a67591e0ae6e6c79b0546744ccf360f3d2c74e85
alobera <albert.jlobera@gmail.com>
2020-05-04 12:02:52 +0200

    done

--- api.py
+++ api.py
@@ -20,7 +20,7 @@ def insertUser(username):
     data = { "Name": username,'Messages':[],'Chats':[]}
     user = db.unique.insert_one(data)
     
-    return f'<h1> Welcome {username}! :) </h1>'
+    return f'<center><h1> Welcome {username}! :)</center> </h1><br><center><img src="https://optinmonster.com/wp-content/uploads/2017/09/perfect-welcome-email-for-new-subscribers.jpg"></center>'
 
 #Assign chat to user.
 @app.route("/chat/<chatname>/user/<username>")
@@ -34,9 +34,9 @@ def creatChat(chatname,username):
         query = db.unique.find({'$and': [{'Name': username},{'Chats': [chatname]}]},{'_id':0})
         if query.count() == 0:
             output =  db.unique.update({'Name': username},{'$set':{'Chats': [chatname]}})
-            return f'<h1> Hello, {username} the new chat "{chatname}" is Created!</h1>'
+            return f'<h1> Hello, {username} the new chat "{chatname}" is Created!</h1><br><center><img src="https://www.lavanguardia.com/r/GODO/LV/p5/WebSite/2018/04/12/Recortada/img_mrubal_20180412-112252_imagenes_lv_otras_fuentes_istock-819245942-kQsG-U442485828453q0E-992x558@LaVanguardia-Web.jpg"></center>''
         else:
-            return f'<h1>Error: you are already in the chat.</h1>'
+            return f'<h1>Error: you are already in the chat.</h1><br><center><img src="https://www.lavanguardia.com/r/GODO/LV/p5/WebSite/2018/04/12/Recortada/img_mrubal_20180412-112252_imagenes_lv_otras_fuentes_istock-819245942-kQsG-U442485828453q0E-992x558@LaVanguardia-Web.jpg"></center>'
 
 
 #Add message to a chat
@@ -47,7 +47,7 @@ def findChatName(chatname, username, message):
         raise Error404 (f'<h1> This user is not registered.</h1>')
     else:
         db.unique.update({'Name': username},{'$set':{"Messages": [message]}})
-        return f'<h1> Thank you for your message! </h1>'
+        return f'<h1><center> Thank you for your message! </center></h1><br><center><img src="https://image.shutterstock.com/image-vector/new-chat-messages-notification-on-260nw-1040460781.jpg"></center>'
 
 
 # Get all message from chat name
@@ -87,8 +87,9 @@ def getAnalysts(chatname):
     for sentence in flat_list:
         return sia.polarity_scores(sentence)
         
-        
-        
+@app.route('/')
+def infoAPI():
+    return f'<center><h1><strong> Welcome to my professional API </strong></h1> <p>My name is Albert and I will explain very quickly the basic commands: <i> <br><p align=center>1. Create a new user to access th chats<br> 2.Join the chat you want to <br>3.Send message and try to be nice because we will know it!<p align=center ></i> <br><h3>For more info about it read my file Readme.md in my Github accound:<br> <strong>[https://github.com/AlbertJlobera/Project-API-]<h3></strong></p></center><br><center><img src=" https://www.kindpng.com/picc/m/226-2265116_team-working-png-png-download-fun-at-work.png"></center> '
 
 app.run("0.0.0.0", PORT, debug=True)
 
